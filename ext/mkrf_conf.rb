753b4be5620d5939208551866e1b5ec1bbbdd47a
Karl Smith <kesmith@godaddy.com>
2018-01-29 09:13:45 -0700

    remove /ext

--- ext/mkrf_conf.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-
-# return an error if openssl isn't installed.
-require 'openssl'
-
-# create dummy rakefile to indicate success
-f = File.open(File.join(File.dirname(__FILE__), "Rakefile"), "w")   
-f.write("task :default\n")
-f.close
