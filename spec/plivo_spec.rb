98db0227d190e94225e883b2ea50c113be7ff704
Karl Smith <kesmith@godaddy.com>
2018-01-29 08:41:04 -0700

    might not be working

--- spec/plivo_spec.rb
+++ spec/plivo_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 RSpec.describe Plivo do
-  let(:auth_id) { 'auth_id' }
-  let(:auth_token) { 'auth_token' }
+  let(:auth_id) { 'SAMWFHNGFHNTJKZJDJOT' }
+  let(:auth_token) { 'MGRlY2JkYWEyZDg3ZWUzZmQ0M2ZmNjI3NzdhYTA0' }
 
   describe 'RestAPI' do
     let(:rest_api) { Plivo::RestAPI.new(auth_id, auth_token) }
@@ -14,6 +14,16 @@ RSpec.describe Plivo do
 
       it 'converts a hash to html parameters' do
         expect(subject).to eq('a=1&b=2')
+      end
+    end
+
+    describe '#get_message' do
+      subject { rest_api.get_message('dde645c7-0c8f-4804-98a5-8b46a9089f62') }
+
+      it 'requests the message status of passed id' do
+        VCR.use_cassette("get_message", match_requests_on: [:method, :uri, :host]) do
+          subject
+        end
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,14 @@
 require 'plivo'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+
+  config.before_record do |interaction|
+    interaction.request['uri'].sub!(/(\/Account\/)(.+?)\//, '\1<account_id>/')
+    interaction.response.body.sub!(/(\/Account\/)(.+?)\//, '\1<account_id>/')
+    interaction.request.headers['Authorization'] = ['Basic blah==']
+  end
+
+end
